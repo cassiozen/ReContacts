a46db5ecffbaf66d49e18a356e64eb9c6ca22179
Cassio Zen <cassiozen@gmail.com>
2025-03-25 11:59:07 -0500

    Add table virtualization

--- src/components/ContactsTable.tsx
+++ src/components/ContactsTable.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
-import { useState } from "react";
+import { createColumnHelper, flexRender, getCoreRowModel, Row, useReactTable } from "@tanstack/react-table";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { useRef, useState } from "react";
 import type { Contact } from "@/db";
 
 // Create a column helper
@@ -12,14 +13,17 @@ const columns = [
   columnHelper.accessor("firstName", {
     cell: (info) => info.getValue(),
     header: "First Name",
+    size: 200,
   }),
   columnHelper.accessor("lastName", {
     cell: (info) => info.getValue(),
     header: "Last Name",
+    size: 200,
   }),
   columnHelper.accessor("email", {
     cell: (info) => info.getValue(),
     header: "Email",
+    size: 300,
   }),
   columnHelper.accessor("createdAt", {
     cell: (info) => {
@@ -27,11 +31,22 @@ const columns = [
       return date instanceof Date ? date.toLocaleString() : "N/A";
     },
     header: "Created At",
+    size: 200,
   }),
 ];
 
 export default function ContactsTable({ contacts }: { contacts: Contact[] }) {
   const [data] = useState(contacts);
+  //we need a reference to the scrolling element for logic down below
+  const tableContainerRef = useRef<HTMLDivElement>(null);
+
+  const rowVirtualizer = useVirtualizer({
+    count: data.length,
+    estimateSize: () => 33, //estimate row height for accurate scrollbar dragging
+    getScrollElement: () => tableContainerRef.current,
+    measureElement: (element) => element?.getBoundingClientRect().height,
+    overscan: 5,
+  });
 
   const table = useReactTable({
     data,
@@ -39,36 +54,75 @@ export default function ContactsTable({ contacts }: { contacts: Contact[] }) {
     getCoreRowModel: getCoreRowModel(),
   });
 
+  const { rows } = table.getRowModel();
+
   return (
-    <div className="overflow-x-auto rounded-md border">
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th
-                  key={header.id}
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+    <div className="">
+      <div className="rounded-md border overflow-x-auto h-[600px] max-w-4xl " ref={tableContainerRef}>
+        <table className="w-full grid divide-y divide-gray-200">
+          <thead className="bg-gray-50 sticky top-0 z-10">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    key={header.id}
+                    scope="col"
+                    style={{
+                      width: header.getSize(),
+                    }}
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    {flexRender(header.column.columnDef.header, header.getContext())}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+
+          <tbody
+            style={{
+              display: "grid",
+              height: `${rowVirtualizer.getTotalSize()}px`, //tells scrollbar how big the table is
+              position: "relative", //needed for absolute positioning of rows
+            }}
+            className="bg-white divide-y divide-gray-200"
+          >
+            {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+              const row = rows[virtualRow.index] as Row<Contact>;
+              return (
+                <tr
+                  data-index={virtualRow.index} //needed for dynamic row height measurement
+                  ref={(node) => rowVirtualizer.measureElement(node)} //measure dynamic row height
+                  key={row.id}
+                  style={{
+                    display: "flex",
+                    position: "absolute",
+                    transform: `translateY(${virtualRow.start}px)`, //this should always be a `style` as it changes on scroll
+                    width: "100%",
+                  }}
+                  className="hover:bg-gray-50"
                 >
-                  {flexRender(header.column.columnDef.header, header.getContext())}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id} className="hover:bg-gray-50">
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id} className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <td
+                        key={cell.id}
+                        style={{
+                          display: "flex",
+                          width: cell.column.getSize(),
+                          maxWidth: cell.column.getSize(),
+                        }}
+                        className="px-6 py-4 whitespace-nowrap truncate text-sm text-gray-500"
+                      >
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
